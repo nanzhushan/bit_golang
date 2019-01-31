a401fbcca24e65c68ebb8a747a3acc49f1beb8e6
knight <knight@qq.com>
2019-01-31 21:28:38 +0800

    aaa

---
+++ 练习/测试import功能/src/lea1/lea.go
@@ -0,0 +1,11 @@
+package lea1
+
+import "fmt"
+
+func init()  {
+	fmt.Errorf("hello this is lea1")
+}
+
+func A()  {
+	
+}
---
+++ 练习/测试import功能/src/show2/show.go
@@ -0,0 +1,12 @@
+package show2
+
+import "fmt"
+
+func init()  {
+	fmt.Print("hello, this is show1")
+}
+
+
+func Show()  {
+	
+}
