5e024ec4aed5e67f80040e02722822111a0fc085
knight <knight@qq.com>
2019-02-01 12:41:46 +0800

    Add

---
+++ go小功能/发送get请求/get.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"io/ioutil"
+)
+
+func httpGet() {
+	resp, err := http.Get("http://www.01happy.com/demo/accept.php?id=1")
+	if err != nil {
+		// handle error
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+	}
+
+	fmt.Println(string(body))
+}
+
+func main()  {
+	httpGet()
+}
