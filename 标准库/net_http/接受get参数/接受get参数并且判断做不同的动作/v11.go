ec629387038bfa86fbd53fb81f0b6c5708fa169a
xiaozhou <xiaozhou@qq.com>
2019-03-17 16:01:24 +0800

    addyy

---
+++ 标准库/net_http/接受get参数/接受get参数并且判断做不同的动作/v11.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	res := r.URL.Query()   // map 字典类型
+	name := res["name"]
+	//fmt.Print(reflect.TypeOf(name))   // 是一个字符串的数组
+	if name[0] == "crm-web"{
+		fmt.Print("1")
+	}else if name[0] == "crm" {
+		fmt.Print("2")
+	}else if name[0] == "sc-crm" {
+		fmt.Print("3")
+	}else {
+		fmt.Print("args not match")
+	}
+
+
+	fmt.Fprint(w,"hello")     // 前端返回输出
+
+}
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.ListenAndServe("0.0.0.0:8000", nil)
+}
