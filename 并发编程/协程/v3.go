eaa70e50c74aba593469af69421f628f1fcfd059
xiaozhou <xiaozhou@qq.com>
2019-04-17 20:45:13 +0800

    update

--- 并发编程/协程/v3.go
+++ 并发编程/协程/v3.go
@@ -12,7 +12,11 @@ func main()  {
 		fmt.Println("1")
 
 	}(i)
-	fmt.Println("2")    // 这是主协程
-	time.Sleep(1* time.Second)
+	fmt.Println("2")    
+	time.Sleep(1* time.Second)    // 主协程里面一般进行sleep 而不马上退出 留时间给子协程运行
 }
 
+
+
+// 程序会优先执行主协程，当主线程执行完成之后 程序会立刻退出，没有多余的时间去执行子协程
+// 如果在程序的最后让主线程休眠1秒钟，程序就有时间取执行子协程
