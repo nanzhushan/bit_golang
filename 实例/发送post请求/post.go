0873af3ee8c0190e142789f57cb0c97ce5f35c86
xiaozhou <xiaozhou@qq.com>
2019-04-20 10:57:32 +0800

    g

--- 实例/发送post请求/post.go
+++ 实例/发送post请求/post.go
@@ -9,12 +9,12 @@ import (
 )
 
 func httpPost() {
-	postData := strings.NewReader(`{"name":"knight"}`)
+	postData := strings.NewReader(`{"name":"knight"}`)   // 使用strings库将其转换成io.Reader流 为传输做好准备
 	res, err := http.Post("https://httpbin.org/post", "application/json", postData)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer res.Body.Close()
+	defer res.Body.Close()   // 客户端读取完所有数据之后 把连接进行关闭
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		log.Fatal(err)
