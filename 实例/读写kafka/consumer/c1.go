10f852057c2bb3b9c2845d8af2f5976635ac06fc
knight <knight@qq.com>
2019-05-10 13:53:04 +0800

    5555555555

---
+++ 实例/读写kafka/consumer/c1.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Shopify/sarama"
+)
+
+func main() {
+	consumer_test()
+}
+
+func consumer_test() {
+	fmt.Printf("consumer test.....")
+
+	config := sarama.NewConfig()
+	config.Consumer.Return.Errors = true
+	config.Version = sarama.V0_11_0_2
+
+	// consumer
+	consumer, err := sarama.NewConsumer([]string{"192.168.100.12:9092"}, config)
+	if err != nil {
+		fmt.Printf("consumer_test create consumer error %s\n", err.Error())
+		return
+	}
+
+	defer consumer.Close()
+
+	partition_consumer, err := consumer.ConsumePartition("xxoo", 0, sarama.OffsetOldest)
+	if err != nil {
+		fmt.Printf("try create partition_consumer error %s\n", err.Error())
+		return
+	}
+	defer partition_consumer.Close()
+
+	for {
+		select {
+		case msg := <-partition_consumer.Messages():
+			fmt.Printf("msg value: %s\n", string(msg.Value))
+		case err := <-partition_consumer.Errors():
+			fmt.Printf("err :%s\n", err.Error())
+		}
+	}
+
+	// for {
+	// 	select {
+	// 	case msg := <-partition_consumer.Messages():
+	// 		fmt.Printf("msg offset: %d, partition: %d, timestamp: %s, value: %s\n",
+	// 			msg.Offset, msg.Partition, msg.Timestamp.String(), string(msg.Value))
+	// 	case err := <-partition_consumer.Errors():
+	// 		fmt.Printf("err :%s\n", err.Error())
+	// 	}
+	// }
+
+}
